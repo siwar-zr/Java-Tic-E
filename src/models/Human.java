1878bdd4147484af7502a3e4e318904f8f95e392
siwar-zr <siwarzrelli180959@gmail.com>
2025-02-21 09:51:46 +0100

    encapsulation

---
+++ src/models/Human.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Human {
+
+    private String name;
+    private int age;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
